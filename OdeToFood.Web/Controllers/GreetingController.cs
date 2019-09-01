d491fc1391c682e6a76fe3259171689565aaf673
khins <kevin@hinshawfam.com>
2019-09-01 11:10:48 -0500

    adds appsetiings key and viewmodel

--- OdeToFood.Web/Controllers/GreetingController.cs
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OdeToFood.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +13,9 @@ namespace OdeToFood.Web.Controllers
         // GET: Greeting
         public ActionResult Index()
         {
-            return View();
+            var model = new GreetingViewModel();
+            model.Message = ConfigurationManager.AppSettings["message"];
+            return View(model);
         }
     }
 }
