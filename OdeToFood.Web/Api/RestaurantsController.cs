24a8d06c3bb69c8136d5c6b2804c31c7a255e545
khins <kevin@hinshawfam.com>
2019-09-02 07:50:33 -0500

    Understanding MVC and API Controllers

---
+++ OdeToFood.Web/Api/RestaurantsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace OdeToFood.Web.Api
+{
+    public class RestaurantsController : ApiController
+    {
+        public string Get()
+        {
+            return "hello world";
+        }
+    }
+}
